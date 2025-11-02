c84933e5bc43e529331e2c3f6b9f5d8254d91acb
PhatDylan <phatnguyentan2205@gmail.com>
2025-11-02 21:42:10 +0700

    Update Window.xaml and xaml.cs

--- FlappyBird.Data/obj/Debug/net9.0/FlappyBird.Data.AssemblyInfo.cs
+++ FlappyBird.Data/obj/Debug/net9.0/FlappyBird.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlappyBird.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a28e97cd17c785c0af6bbabdca243a1c7ace4ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlappyBird.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlappyBird.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
